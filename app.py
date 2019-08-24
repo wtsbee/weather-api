e34b0b8ba9bb64748d15d18c938eab307bb45c09
wtsbee <atsushi@wataseatsushinoMacBook-Air.local>
2019-08-24 23:07:39 +0900

    雨の日の判定元データの変更

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def post_request():
     sumd = {} # 辞書型を初期化
     for row in df.iterrows():
         ymd = row[1]["年月日"]
-        weather = row[1]["天気概況(昼：06時〜18時)"]
+        kousui = row[1]["降水量の合計(mm)"]
         md = re.sub(r'\d{4}\/', '', ymd) # 月/日だけにする
         # 年を閏年の2016年で仮置きする
         md = '2016/' + md
@@ -23,16 +23,14 @@ def post_request():
             sumd[md] = [0, 0, 0, 0, 0]
         # 列の値を加算する
         for i in range(1, 5):
-            sumd[md][i] += row[1][i + 1]
+            sumd[md][i] += row[1][i]
         # 雨の日の場合に1を加算
-        # print(sumd)
-        # break
-        if weather.find("雨") >= 0:
+        if kousui > 0:
             sumd[md][0] += 1
     
     # 表として出力するための処理
     df = pd.DataFrame.from_dict(sumd).T
-    df.columns = ("6〜18時に雨が降った日数(日)","1日の降水量(mm)","最高気温(℃)","最低気温(℃)","平均気温(℃)")
+    df.columns = ("雨の日数(日)","1日の降水量(mm)","最高気温(℃)","最低気温(℃)","平均気温(℃)")
 
     df_r = df.reset_index()
     df_r = df_r.rename(columns = {'index':'日付'})
