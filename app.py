4af117dc7eb00cbe9a2f36860199c29fa32e4dbf
wtsbee <atsushi@wataseatsushinoMacBook-Air.local>
2019-09-01 23:14:40 +0900

    Heroku対応

--- app.py
+++ app.py
@@ -73,4 +73,4 @@ def post_request():
     return jsonify({"header": header, "record": record})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port = 8000, debug=False)
+    app.run(host='0.0.0.0',debug=False)
